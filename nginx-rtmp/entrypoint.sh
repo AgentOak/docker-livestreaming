64d136599ed20dab5192b6c4b6c710758986d3be
Jan Erik Petersen <JanErikPetersen@web.de>
2020-05-15 01:31:26 +0200

    Adjust HLS variants

--- nginx-rtmp/entrypoint.sh
+++ nginx-rtmp/entrypoint.sh
@@ -6,8 +6,21 @@ if ! getent passwd app >/dev/null 2>&1; then
     adduser -S -u "$APP_UID" -G app -h /var/lib/nginx -g "" -s /usr/sbin/nologin -D -H app
 fi
 
-mkdir -p /srv/segments/dash /srv/segments/hls /srv/segments/image /srv/recordings
-chown app: /srv/segments /srv/segments/dash /srv/segments/hls /srv/segments/image /srv/recordings
+create_dir() {
+    dirname="$1"
+
+    if [ ! -d "$dirname" ]; then
+        mkdir "$dirname"
+        chown app: "$dirname"
+        chmod 755 "$dirname"
+    fi
+}
+
+create_dir /srv/segments
+create_dir /srv/segments/dash
+create_dir /srv/segments/hls
+create_dir /srv/segments/image
+create_dir /srv/recordings
 
 if [ ! -f "/etc/nginx/nginx.conf" ]; then
     envsubst '$NGINX_WORKER_PROCESSES $NGINX_WORKER_CONNECTIONS $RTMP_MAX_CONNECTIONS $HTTP_LISTEN $RTMP_LISTEN' </etc/nginx/nginx.conf.tpl >/etc/nginx/nginx.conf
